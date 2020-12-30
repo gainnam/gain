1af89be9126a9ff0171afef8ca16a32196da0a50
wogus <wogus@LAPTOP-HRTN1DGF>
2020-12-30 09:43:25 +0900

    20201230 1교시 중 HelloWorld오류 수정

--- src/test/java/kr/or/test/HelloWorldApp.java
+++ src/test/java/kr/or/test/HelloWorldApp.java
@@ -2,11 +2,12 @@ package kr.or.test;
 
 public class HelloWorldApp {
 
+	private static final int MemberVO = 0;
 	public static void main(String[] args) {
 		// 위 main()메서드는 자바프로그램의 실행 진입지점 입니다.
 		System.out.println("헬로우 월드!");
 		MemberVO memberVO = new MemberVO();
-		sumFunc(memberVO, 4);//플러스함수를 실행할 때 매개변수 값을 줍니다.
+		sumFunc(MemberVO, 4);//플러스함수를 실행할 때 매개변수 값을 줍니다.
 		sumFunc(3,4);
 		//매서드의 특징, 위와 같이 같은메서드명 로드된 매개변수가 다른 방식 오버로드된 메서드라고 합니다.
 	}
