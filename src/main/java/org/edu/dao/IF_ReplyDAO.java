d9140ce7b086f74620f341c417491325132b0f83
User <User@DESKTOP-IKNVUE2>
2021-01-07 15:37:14 +0900

    RestAPI 댓글 CRUD마무리

--- src/main/java/org/edu/dao/IF_ReplyDAO.java
+++ src/main/java/org/edu/dao/IF_ReplyDAO.java
@@ -2,12 +2,15 @@ package org.edu.dao;
 
 import java.util.List;
 
+import org.edu.vo.PageVO;
 import org.edu.vo.ReplyVO;
 
 public interface IF_ReplyDAO {
-	public List<ReplyVO> selectReply(Integer bno);
-	public int countReply(Integer bno);
-	public void insertReply(ReplyVO replyVO);
-	public void updateReply(ReplyVO replyVO);
-	public void deleteReply(Integer rno);
+	public List<ReplyVO> selectReply(Integer bno, PageVO pageVO) throws Exception;
+	public void updateCountReply(Integer bno) throws Exception;
+	public void insertReply(ReplyVO replyVO) throws Exception;
+	public void updateReply(ReplyVO replyVO) throws Exception;
+	public void deleteReply(Integer rno) throws Exception;
+	public int selectReplyCount(Integer bno) throws Exception;
+	public void deleteReplyAll(Integer bno) throws Exception;
 }
